51c69ba2bb41c817a9a77f2ca4b33724c1d7e5ab
antonyalexcm <antonyalexcm@gmail.com>
2020-01-07 12:38:11 +0530

    added: logic for division of 2 numbers

--- src/com/bridgelabz/practice/Calculator.java
+++ src/com/bridgelabz/practice/Calculator.java
@@ -9,28 +9,35 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Enter how many numbers you want to add");
-		int count = scanner.nextInt();
-		
-		while(count < 1) {
-			System.out.println("Enter a positive whole number");
-			count = scanner.nextInt();
-		}
-		System.out.println("Entered count is "+count);
-
-		int[] arr = new int[count];
-		
-		for(int i=0; i<arr.length ; i++) {
-			System.out.println("Enter number "+(i+1));
-			arr[i] = scanner.nextInt();
-		}
-		
-		int sum = Utility.add(arr);		
-		System.out.println("Sum of "+count+" entered numbers is = "+sum);
+//		System.out.println("Enter how many numbers you want to add");
+//		int count = scanner.nextInt();
+//		
+//		while(count < 1) {
+//			System.out.println("Enter a positive whole number");
+//			count = scanner.nextInt();
+//		}
+//		System.out.println("Entered count is "+count);
+//
+//		int[] arr = new int[count];
+//		
+//		for(int i=0; i<arr.length ; i++) {
+//			System.out.println("Enter number "+(i+1));
+//			arr[i] = scanner.nextInt();
+//		}
+//		
+//		int sum = Utility.add(arr);		
+//		System.out.println("Sum of "+count+" entered numbers is = "+sum);
 		
 //		long product = Utility.multiply(arr);		
 //		System.out.println("Product of "+count+" entered numbers is = "+product);
 		
+		System.out.println("enter 1st number for division");
+		int num1 = scanner.nextInt();
+		
+		System.out.println("enter 2nd number for division");
+		int num2 = scanner.nextInt();
+		
+		System.out.println("Division of entered numbers is "+Utility.divide(num1, num2));
 		scanner.close();
 	}
 }
--- src/com/bridgelabz/utility/Utility.java
+++ src/com/bridgelabz/utility/Utility.java
@@ -21,4 +21,16 @@ public class Utility {
 		System.out.println("Received product is "+product);
 		return product;
 	}
+	
+	public static double divide(int number1, int number2) {
+		double value = 0;
+
+		try {
+			value = number1 / (number2*1.0);
+			return value;
+		} catch (Exception e) {
+			System.out.println("Use a number other than 0 in the denominator");
+		}
+		throw new ArithmeticException("Use a number other than 0 in the denominator");
+	}
 }
